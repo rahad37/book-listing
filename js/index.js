21dcc7f3c0b8eebfc14a96bea73de7fe93d0dede
Rahad Ahmed <rrahad7867@gmail.com>
2021-11-22 20:07:05 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+let form = document.getElementById('book-form');
+let bookList = document.querySelector('#book-list');
+
+bookList.addEventListener('click', removeBook);
 
 class Book{
     constructor(title, author, isbn){
@@ -5,54 +9,100 @@ class Book{
         this.author = author,
         this.isbn = isbn
     }
-   
+}
+
+// Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static addBook(book){
+        let books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static displayBooks(){
+        let books = Store.getBooks();
+        books.forEach(book => {
+            UI.addBook(book);
+        })
+    }
+    static remove(isbn){
+        let books = Store.getBooks();
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        })
+        localStorage.setItem('books', JSON.stringify(books));
+    }
 }
 
 class UI{
-    constructor(){
-
+    static addBook(book){
+        let bookList = document.getElementById('book-list');
+        let list = document.createElement('tr');
+        list.innerHTML =
+        `<td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href='#' type='button'>Delete</a></td>`
+       bookList.appendChild(list);
     }
-    clearField(){
+    static clearField(){
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
     }
-    addBook(book){
-        let bookList = document.getElementById('book-list');
-        let row = document.createElement('tr');
-        row.innerHTML = 
-        `<td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td><a href='#' type='button'>Delete</a></td>`;
-        bookList.appendChild(row);
+    static showAlert(message, className){
+        let div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.container');
+        let form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove()
+        }, 2000)
     }
-    deleteBook(target){
+    static deleteFromBook(target){
         if(target.hasAttribute('href')){
-            confirm('Are You Sure to delete the book?');
+            confirm('Are you sure to delete the book?');
             target.parentElement.parentElement.remove();
+            Store.remove(target.parentElement.previousElementSibling.textContent.trim());
+            UI.showAlert('Book Removed!', 'error');
         }
     }
-   
 }
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+// FORM...
 
-
-let form = document.getElementById('book-form');
 form.addEventListener('submit', function(e){
     let title = document.getElementById('title').value,
     author = document.getElementById('author').value,
     isbn = document.getElementById('isbn').value
 
-    const book = new Book(title, author, isbn);
-    const ui = new UI();
-    ui.addBook(book);
-    ui.clearField(book);
+    if(title === '' || author === '' || isbn === ''){
+        UI.showAlert('Please fill all the fields!', 'error')
+    }else{
+        const book = new Book(title, author, isbn);
+        UI.addBook(book);
+        UI.showAlert('Book Added!', 'success')
+        UI.clearField();
+        Store.addBook(book);
+    }
     e.preventDefault();
 })
 
-let bookList = document.getElementById('book-list');
-bookList.addEventListener('click', function(e){
-    let ui = new UI();
-    ui.deleteBook(e.target);
+
+function removeBook(e){
+    UI.deleteFromBook(e.target);
     e.preventDefault();
-})
+}
--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
 let form = document.getElementById('book-form');
+let bookList = document.querySelector('#book-list');
 
+bookList.addEventListener('click', removeBook);
 
 class Book{
     constructor(title, author, isbn){
@@ -9,11 +11,41 @@ class Book{
     }
 }
 
-class UI{
-    constructor(){
-
+// Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
     }
-    addBook(book){
+    static addBook(book){
+        let books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+    static displayBooks(){
+        let books = Store.getBooks();
+        books.forEach(book => {
+            UI.addBook(book);
+        })
+    }
+    static remove(isbn){
+        let books = Store.getBooks();
+        books.forEach((book, index) => {
+            if(book.isbn === isbn){
+                books.splice(index, 1);
+            }
+        })
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+class UI{
+    static addBook(book){
         let bookList = document.getElementById('book-list');
         let list = document.createElement('tr');
         list.innerHTML =
@@ -23,21 +55,54 @@ class UI{
         <td><a href='#' type='button'>Delete</a></td>`
        bookList.appendChild(list);
     }
-    clearField(){
+    static clearField(){
         document.getElementById('title').value = '';
         document.getElementById('author').value = '';
         document.getElementById('isbn').value = '';
     }
+    static showAlert(message, className){
+        let div = document.createElement('div');
+        div.className = `alert ${className}`;
+        div.appendChild(document.createTextNode(message));
+        let container = document.querySelector('.container');
+        let form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        setTimeout(() => {
+            document.querySelector('.alert').remove()
+        }, 2000)
+    }
+    static deleteFromBook(target){
+        if(target.hasAttribute('href')){
+            confirm('Are you sure to delete the book?');
+            target.parentElement.parentElement.remove();
+            Store.remove(target.parentElement.previousElementSibling.textContent.trim());
+            UI.showAlert('Book Removed!', 'error');
+        }
+    }
 }
+document.addEventListener('DOMContentLoaded', Store.displayBooks());
+// FORM...
 
 form.addEventListener('submit', function(e){
     let title = document.getElementById('title').value,
     author = document.getElementById('author').value,
     isbn = document.getElementById('isbn').value
 
-    const book = new Book(title, author, isbn);
-    const ui = new UI();
-    ui.addBook(book);
-    ui.clearField();
+    if(title === '' || author === '' || isbn === ''){
+        UI.showAlert('Please fill all the fields!', 'error')
+    }else{
+        const book = new Book(title, author, isbn);
+        UI.addBook(book);
+        UI.showAlert('Book Added!', 'success')
+        UI.clearField();
+        Store.addBook(book);
+    }
     e.preventDefault();
 })
+
+
+function removeBook(e){
+    UI.deleteFromBook(e.target);
+    e.preventDefault();
+}
